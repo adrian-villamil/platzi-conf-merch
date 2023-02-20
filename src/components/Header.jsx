0375a2a1629929b07aef22b465af52363f111a03
adrian-villamil <adrian.villamil@unillanos.edu.co>
2023-02-20 12:25:12 -0500

    Se agregaron link e iconos.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,18 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import '../styles/components/Header.css';
 
 const Header = () => {
   return (
     <div className="Header">
-      <h1 className="Header-title">PlatziConf Merf</h1>
-      <div className="Header-checkout"></div>
+      <h1 className="Header-title">
+        <Link to='/'>PlatziConf Merf</Link>
+      </h1>
+      <div className="Header-checkout">
+        <Link to='/checkout'>
+          <i className="fas fa-shopping-basket fa-2x" />
+        </Link>
+      </div>
     </div>
   );
 };
