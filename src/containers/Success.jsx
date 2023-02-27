d4db9095537945e67a4ff73629b879218163d8b4
adrian-villamil <adrian.villamil@unillanos.edu.co>
2023-02-26 22:14:35 -0500

    Mediante el uso de la latitud y la longitud de la dirección del comprador, podemos renderizar el mapa.

--- src/containers/Success.jsx
+++ src/containers/Success.jsx
@@ -1,14 +1,21 @@
-import React from "react";
+import React, { useContext } from "react";
+import Map from "../components/Map";
+import AppContext from "../context/AppContext";
+import useForwardGeocoding from "../hooks/useForwardGeocoding";
 import '../styles/components/Success.css';
 
 const Success = () => {
+  const { state } = useContext(AppContext);
+  const { buyer } = state;
+  const [lat, lon] = useForwardGeocoding(buyer[0]);
+
   return (
     <div className="Success">
       <div className="Success-content">
-        <h2>Adrian, Gracias por tu compra</h2>
+        <h2>{buyer[0].name}, Gracias por tu compra</h2>
         <span>Tu pedido llegará en 3 días a tu dirección</span>
         <div className="Success-map">
-          Google Maps
+          {lat && lon && <Map lat={lat} lon={lon} />}
         </div>
       </div>
     </div>
